98c0d5a8f15faabfe0d215bc88aa3a4962c0a781
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-05 15:52:55 -0800

    Implement schema tables in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,26 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT PRIMARY KEY,
+  text TEXT,
+  createdAt DATETIME,
+  room_ID INT,
+  FOREIGN KEY(room_ID) references rooms(id),
+  user_ID INT,
+  FOREIGN KEY(user_ID) references users(id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE rooms (
+  id INT PRIMARY KEY,
+  roomName TEXT
+);
 
+CREATE TABLE users(
+  id INT PRIMARY KEY,
+  userName TEXT
+);
 
 
 /*  Execute this file from the command line by typing:
