ce672e1cecc29ab3b2ffa85e2e532dbcf46867b6
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-05 19:47:28 -0800

    Refactor code to utilize middlewar and Express js on request handling

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,31 +1,5 @@
 var models = require('../models');
 
-var headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'application/json'
-};
-
-var collectData = function(req, callback) {
-  var JSONresponse = '';
-  req.on('data', function (chunk) {
-    JSONresponse += chunk;
-  });
-
-  req.on('end', function(err) {
-    if(err) {
-      throw err;
-    }
-    callback(JSONresponse);
-  });
-};
-
-var sendResponse = function(res, data, statusCode) {
-  res.writeHead(headers, statusCode || 200);
-  res.end(data);
-};
 
 
 
@@ -34,8 +8,10 @@ module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
     post: function (req, res) {
-      collectData(req, function(JSONdata) {});
+      var messageObj = req.body;
+      res.send({message: 'message received'});
 
+      console.log(messageObj);
     } // a function which handles posting a message to the database
   },
 
@@ -45,7 +21,10 @@ module.exports = {
 
     // TODO: Send a response
     post: function (req, res) {
-      collectData(req, function(JSONdata) {});
+      var userObj = req.body;
+      res.send({message: 'username recieved'});
+
+      console.log(userObj);
     }
   }
 };
